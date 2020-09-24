e52c1f71490fd749ff4d74159f86486021c6619e
rajnish000anand <rajnish000anand@gmail.com>
2020-09-24 15:12:35 +0530

    adding popUp.js

---
+++ res/popUp.js
@@ -0,0 +1,23 @@
+function popup(txt, col='#ff5500'){
+   
+   let el =document.createElement('div');
+   el.innerHTML=txt;
+   el.className='popuptxt';
+   el.style.color=col;
+   
+   popupC.innerHTML='';
+   popupC.appendChild(el);
+   setTimeout(()=>{try{
+       el.style.bottom='-1em';
+       el.style.opacity='0';
+       setTimeout(()=>{try{
+           popupC.removeChild(el);
+       }catch(err){}},600);
+   }catch(err){}},2000 )
+}
+
+
+let popupC = document.createElement('div');
+popupC.id='popupC';
+document.body.appendChild(popupC);
+document.head.innerHTML+='<style>.popuptxt{position:fixed;bottom:0;left:0;width:100%;text-align:center;background:#ffffff;font-weight:900;line-height:1.5em;border-top:3px solid;transition:0.5s ease-in;opacity:1;}</style>'
